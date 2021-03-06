41d44fe0aa052efc80f9a81bfe5adccf2963cd9a
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2021-03-05 22:12:03 -0500

    Finish bubble-sorting + driver-code

--- src/bubblesorting/BubbleSort.java
+++ src/bubblesorting/BubbleSort.java
@@ -4,7 +4,38 @@ package bubblesorting;
 public class BubbleSort implements SortAlgorithim {
 
     @Override
-    public <T extends Comparable<T>> T[] sort(T[] unsorted)
+    public <T extends Comparable<T>> T[] sort(T[] unsorted) {
+        for (int i = 0, size = array.length; 1 < size -1; ++i) {
+            boolean swapper = false;
+            for (int j = 0; j < size -1 - i; ++j) {
+                if (greater(array[j], array[j +1])) {
+                    swap(array, j, j +1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        return array;
+    }
 
+    public static void main(string) {
+
+        Integer[] integers = {4, 23, 6, 78, 1, 54, 231, 9, 12};
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sort(integers);
+
+        for (int i = 0; i < integers.length - 1; ++i) {
+            assert integers[i] <= integers[i + 1];
+        }
+        print(integers);
+
+        String[] strings = {"c", "a", "e", "b", "d"};
+        bubbleSort.sort(strings);
+        for (int i = 0; i < strings.length - 1; i++) {
+            assert strings[i].compareTo(strings[i + 1]) <= 0;
+        }
+        print(bubbleSort.sort(strings));
     }
 }
